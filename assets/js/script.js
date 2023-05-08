3c0d0465293e1e4442002297a2f8c020183d4f9b
kitkatyourmom <sarahlridings@gmail.com>
2023-05-07 22:19:37 -0600

    add timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 const startButton = document.getElementById("start-btn");
 const nextButton = document.getElementById("next-btn");
-
 const questionContainer = document.getElementById("question-container");
 const questionElement = document.getElementById("question");
 const answerButtonsElement = document.getElementById("answer-buttons");
@@ -8,32 +7,56 @@ const answerButtonsElement = document.getElementById("answer-buttons");
 let shuffledQuestions,currentQuestionIndex;
 let quizScore = 0;
 
-
-startButton.addEventListener("click", startGame)
+startButton.addEventListener("click", startGame);
 
 nextButton.addEventListener("click", () =>{
-    currentQuestionIndex++
-    setNextQuestion()
+    currentQuestionIndex++;
+    setNextQuestion();
 });
 
+// function startTimer(duration, display) {
+//     var timer = duration, minutes, seconds;
+//     setInterval(function () {
+//         minutes = parseInt(timer / 60, 10);
+//         seconds = parseInt(timer % 60, 10);
+
+//         minutes = minutes < 10 ? "0" + minutes : minutes;
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
+
+//         display.textContent = minutes + ":" + seconds;
+
+//         if (--timer < 0) {
+//             timer = duration;
+//         }
+//     }, 1000);
+// }
+
+// window.onload = function () {
+//     var oneMinute = 60 * 1,
+//         display = document.querySelector("#timer");
+//     startTimer(oneMinute, display);
+// };
+
 function startGame() {
-    startButton.classList.add("hide")
-    shuffledQuestions = questions.sort(() =>Math.random() -0.5);
-    currentQuestionIndex = 0;
+    startButton.classList.add("hide");
     questionContainer.classList.remove("hide")
     setNextQuestion()
+
+    shuffledQuestions = questions.sort(() =>Math.random() -0.5);
+    currentQuestionIndex = 0;
+
     quizScore = 0
 }
 
-
 function setNextQuestion() {
     resetState();
-    showQuestion(shuffledQuestions[currentQuestionIndex])
+    // showQuestion();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
 function showQuestion(question) {
     questionElement.innerText = question.question;
-    question.answers.forEach((answer) =>{
+    question.answers.forEach((answer) => {
         const button = document.createElement("button");
         button.innerText = answer.text;
         button.classList.add("btn");
@@ -45,15 +68,13 @@ function showQuestion(question) {
     })
 }
 
-
 function resetState() {
-    clearStatusClass(document.body);
     nextButton.classList.add("hide");
+    clearStatusClass(document.body);
     while(answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild);
     }
 }
-
 
 function selectAnswer(e){
     const selectedButton = e.target;
@@ -88,84 +109,84 @@ function setStatusClass(element, correct) {
     }
 }
 
-
 function clearStatusClass(element){
     element.classList.remove("correct");
     element.classList.remove("wrong")
 }
 
 const questions = [
-    {
-        question: "What was Animal Kingdom\'s opening day?",
-        answers : [
-            {text: "April 22, 1998", correct: true},
-            {text: "July 7, 1997", correct: false},
-            {text: "June 21, 1995", correct: false},
-            {text: "May 1, 2000", correct: false},
-        ],
-    },
-    {
-        question: "Who was the Disney Imagineer that headed the Animal Kingdom project?",
-        answers : [
-            {text: "Joe Rohde", correct: true},
-            {text: "Mary Blair", correct: false},
-            {text: "Roger E. Broggie", correct: false},
-            {text: "Lori Coltrin", correct: false},
-        ],
-    },
-    {
-        question: "How many miles did the team of Imageneers travel in search of the essential look of life in the wild?",
-        answers : [
-            {text: "500,000 miles", correct: true},
-            {text: "350,000 miles", correct: false},
-            {text: "100,000 miles", correct: false},
-            {text: "250,000 miles", correct: false},
-        ],
-    },
-    {
-        question: "How long did it take the ten artists and three Imagineers to create the tree of life?",
-        answers : [
-            {text: "18 months", correct: true},
-            {text: "9 months", correct: false},
-            {text: "20 months", correct: false},
-            {text: "4 months", correct: false},
-        ],
-    },
-    {
-        question: "How many animal carvings are on the tree of life?",
-        answers : [
-            {text: "337", correct: true},
-            {text: "432", correct: false},
-            {text: "128", correct: false},
-            {text: "224", correct: false},
-        ],
-    },
-    {
-        question: "How many floating mountains are there in the Valley of Mo\'ara on Pandora?",
-        answers : [
-            {text: "22", correct: true},
-            {text: "18", correct: false},
-            {text: "8", correct: false},
-            {text: "12", correct: false},
-        ],
-    },
-    {
-        question: "What animal was first born at Disney\'s Animal Kingdom?",
-        answers : [
-            {text: "A Kudu", correct: true},
-            {text: "An Elephant", correct: false},
-            {text: "An Okapi", correct: false},
-            {text: "A Gorilla", correct: false},
-        ],
-    },
-    {
-        question: "Since the park opened, how much money has the Disney Conservation Fund raised to support nonprofit organizations working with communities to reverse the decline of wildlife?",
-        answers : [
-            {text: "$120 million", correct: true},
-            {text: "$80 million", correct: false},
-            {text: "$100 million", correct: false},
-            {text: "$60 million", correct: false},
-        ],
-    },
+        {
+            question: "What was Animal Kingdom\'s opening day?",
+            answers : [
+                {text: "April 22, 1998", correct: true},
+                {text: "July 7, 1997", correct: false},
+                {text: "June 21, 1995", correct: false},
+                {text: "May 1, 2000", correct: false},
+            ],
+        },
+        {
+            question: "Who was the Disney Imagineer that headed the Animal Kingdom project?",
+            answers : [
+                {text: "Joe Rohde", correct: true},
+                {text: "Mary Blair", correct: false},
+                {text: "Roger E. Broggie", correct: false},
+                {text: "Lori Coltrin", correct: false},
+            ],
+        },
+        {
+            question: "How many miles did the team of Imageneers travel in search of the essential look of life in the wild?",
+            answers : [
+                {text: "500,000 miles", correct: true},
+                {text: "350,000 miles", correct: false},
+                {text: "100,000 miles", correct: false},
+                {text: "250,000 miles", correct: false},
+            ],
+        },
+        {
+            question: "How long did it take the ten artists and three Imagineers to create the tree of life?",
+            answers : [
+                {text: "18 months", correct: true},
+                {text: "9 months", correct: false},
+                {text: "20 months", correct: false},
+                {text: "4 months", correct: false},
+            ],
+        },
+        {
+            question: "How many animal carvings are on the tree of life?",
+            answers : [
+                {text: "337", correct: true},
+                {text: "432", correct: false},
+                {text: "128", correct: false},
+                {text: "224", correct: false},
+            ],
+        },
+        {
+            question: "How many floating mountains are there in the Valley of Mo\'ara on Pandora?",
+            answers : [
+                {text: "22", correct: true},
+                {text: "18", correct: false},
+                {text: "8", correct: false},
+                {text: "12", correct: false},
+            ],
+        },
+        {
+            question: "What animal was first born at Disney\'s Animal Kingdom?",
+            answers : [
+                {text: "A Kudu", correct: true},
+                {text: "An Elephant", correct: false},
+                {text: "An Okapi", correct: false},
+                {text: "A Gorilla", correct: false},
+            ],
+        },
+        {
+            question: "Since the park opened, how much money has the Disney Conservation Fund raised to support nonprofit organizations working with communities to reverse the decline of wildlife?",
+            answers : [
+                {text: "$120 million", correct: true},
+                {text: "$80 million", correct: false},
+                {text: "$100 million", correct: false},
+                {text: "$60 million", correct: false},
+            ],
+        },
+    
+    ]
 
-]
